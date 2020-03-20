b92bdf1079de65314dc219af8a9c1dc4e3d56d16
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-20 19:38:50 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BLoC Infinite List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'BLoC Infinite List'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello world !',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
